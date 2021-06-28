c5a1610e810048fcf09dd9b84e260f4c16b5b05c
AndreyPomelov <k-udm@ya.ru>
2021-06-28 22:00:27 +0400

    Lesson 11 Homework

--- thymeleaf/src/main/java/com/example/thymeleaf/controller/MainController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.thymeleaf.controller;
 
 import com.example.thymeleaf.model.entity.Product;
 import com.example.thymeleaf.model.repository.ProductRepository;
+import com.example.thymeleaf.model.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,12 +18,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class MainController {
 
-    /** Ссылка на репозиторий, осуществляющий взаимодействие с БД */
+    /** Ссылки на репозитории, осуществляющие взаимодействие с БД */
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     /** Конструктор */
-    public MainController(ProductRepository productRepository) {
+    public MainController(ProductRepository productRepository, UserRepository userRepository) {
         this.productRepository = productRepository;
+        this.userRepository = userRepository;
     }
 
     /** Метод, принимающий гет-запрос по корневому пути
@@ -34,6 +37,17 @@ public class MainController {
     public String index(Model model) {
         model.addAttribute("products", productRepository.findAll());
         return "index";
+    }
+
+    /** Метод, принимающий гет-запрос перехода на страницу пользователей
+     *
+     * @param model модель, в которую передаётся список пользователей
+     * @return имя страницы, передаваемой клиенту
+     */
+    @RequestMapping(value = "users", method = RequestMethod.GET)
+    public String users(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users";
     }
 
     /** Метод, принимающий гет-запрос перехода на страницу
@@ -50,6 +64,20 @@ public class MainController {
         return "add";
     }
 
+    /** Метод, принимающий гет-запрос перехода на страницу
+     *
+     * с формой для редактирования продукта
+     * @param model модель, в которую передаётся пустой объект
+     *              для заполнения его полей через форму
+     * @return имя страницы с формой
+     */
+    @RequestMapping(value = "update", method = RequestMethod.GET)
+    public String update(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "update";
+    }
+
     /** Метод, принимающий пост-запрос с данными из заполненной формы
      *
      * @param product экземпляр класса Product с заполненными полями
@@ -58,8 +86,21 @@ public class MainController {
      */
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String save(Product product) {
+        product.setId(0L);
         productRepository.save(product);
         return "add";
+    }
+
+    /** Метод, принимающий пост-запрос с данными для редактирования объекта
+     *
+     * @param product экземпляр класса Product с заполненными полями
+     * @see Product
+     * @return имя страницы с формой
+     */
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public String updateProduct(Product product) {
+        productRepository.save(product);
+        return "update";
     }
 
     /** Метод, принимающий гет-запрос для удаления продукта из БД
